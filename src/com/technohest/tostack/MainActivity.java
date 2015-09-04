30e8069fd5a35b39c3f132adb5d4b0583f69662d
oskar367 <oskar.jedvert@gmail.com>
2015-09-04 12:32:08 +0200

    added bunch of shit

--- src/com/technohest/tostack/MainActivity.java
+++ src/com/technohest/tostack/MainActivity.java
@@ -1,9 +1,16 @@
 package com.technohest.tostack;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    private ArrayAdapter<String> adapter;
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +18,41 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.todolist);
+        ListView todolist = (ListView) findViewById(R.id.todolist);
+        todolist.setAdapter(adapter);
+
+        initButtons();
+    }
+
+    private void initButtons() {
+        Button pop = (Button) findViewById(R.id.popbutton);
+        pop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pop(v);
+            }
+        });
+        Button stash = (Button) findViewById(R.id.stashbutton);
+        stash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stash(v);
+            }
+        });
+    }
+
+    private void stash(View v) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        toast("Stash");
+    }
+
+    private void pop(View v) {
+        toast("POP!");
+    }
+
+    public void toast(String string){
+        Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ src/com/technohest/tostack/TodoAdapter.java
@@ -0,0 +1,72 @@
+package com.technohest.tostack;
+
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+
+/**
+ * @author Oskar Jedvert
+ * Created by oskar on 2015-09-04.
+ */
+public class TodoAdapter implements ListAdapter {
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
